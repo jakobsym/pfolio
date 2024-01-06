b60075eaf96aaff1e8a8274ae5a53dedec031a29
jakobsym <jakob.symchych@my.utsa.edu>
2024-01-06 09:55:59 -0700

    created structures for Portfolio

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,5 +1,7 @@
 package cmd
 
+import "time"
+
 // TODO: decide on 1 or more portfolios
 // Make multiple portfolios (1 per chain?)
 type Portfolio []struct {
@@ -7,4 +9,8 @@ type Portfolio []struct {
 }
 
 type Position struct {
+	Ticker     string
+	Size       float64
+	EntryPrice float64
+	EntryTime  time.Time // time.Now().UTC() for instantiated
 }
