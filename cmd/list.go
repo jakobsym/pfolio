1b2acef830549be3233f8afe6d0d2202508373f1
jakobsym <jakob.symchych@my.utsa.edu>
2024-01-06 09:38:06 -0700

    init cl args of app

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,13 +12,8 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Displays your portfolio.",
+	Long:  `Display your portfolio, showing all positions (size/entry).`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("list called")
 	},
